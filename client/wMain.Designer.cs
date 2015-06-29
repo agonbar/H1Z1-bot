60dc70dd22c038827b3f1fa743264ccbc935b3be
Aitor González Fernández <reimashi@gmail.com>
2015-06-29 15:07:33 +0200

    Añadida ventana principal del bot

---
+++ client/wMain.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace H1Z1Bot
+{
+    partial class wMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbInfo = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lConnection = new System.Windows.Forms.Label();
+            this.lDoorname = new System.Windows.Forms.Label();
+            this.lKeys = new System.Windows.Forms.Label();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.gbInfo.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbInfo
+            // 
+            this.gbInfo.Controls.Add(this.label4);
+            this.gbInfo.Controls.Add(this.label3);
+            this.gbInfo.Controls.Add(this.label2);
+            this.gbInfo.Controls.Add(this.label1);
+            this.gbInfo.Location = new System.Drawing.Point(12, 12);
+            this.gbInfo.Name = "gbInfo";
+            this.gbInfo.Size = new System.Drawing.Size(344, 73);
+            this.gbInfo.TabIndex = 0;
+            this.gbInfo.TabStop = false;
+            this.gbInfo.Text = "Información";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lKeys);
+            this.groupBox2.Controls.Add(this.lDoorname);
+            this.groupBox2.Controls.Add(this.lConnection);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(344, 95);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Estado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "F11";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "F12";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "- Inicia el bot";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "- Para el bot";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Conexión:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Puerta:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Claves:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbLog);
+            this.groupBox1.Location = new System.Drawing.Point(12, 192);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(344, 161);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log";
+            // 
+            // lConnection
+            // 
+            this.lConnection.AutoSize = true;
+            this.lConnection.Location = new System.Drawing.Point(76, 26);
+            this.lConnection.Name = "lConnection";
+            this.lConnection.Size = new System.Drawing.Size(0, 13);
+            this.lConnection.TabIndex = 3;
+            // 
+            // lDoorname
+            // 
+            this.lDoorname.AutoSize = true;
+            this.lDoorname.Location = new System.Drawing.Point(76, 48);
+            this.lDoorname.Name = "lDoorname";
+            this.lDoorname.Size = new System.Drawing.Size(0, 13);
+            this.lDoorname.TabIndex = 4;
+            // 
+            // lKeys
+            // 
+            this.lKeys.AutoSize = true;
+            this.lKeys.Location = new System.Drawing.Point(76, 70);
+            this.lKeys.Name = "lKeys";
+            this.lKeys.Size = new System.Drawing.Size(0, 13);
+            this.lKeys.TabIndex = 5;
+            // 
+            // tbLog
+            // 
+            this.tbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbLog.Location = new System.Drawing.Point(3, 16);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbLog.Size = new System.Drawing.Size(338, 142);
+            this.tbLog.TabIndex = 0;
+            // 
+            // wMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 359);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.gbInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "wMain";
+            this.Text = "H1Z1 - Brute-force Bot";
+            this.gbInfo.ResumeLayout(false);
+            this.gbInfo.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbInfo;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lConnection;
+        private System.Windows.Forms.Label lKeys;
+        private System.Windows.Forms.Label lDoorname;
+        private System.Windows.Forms.TextBox tbLog;
+    }
+}
+
---
+++ client/wMain.cs
@@ -0,0 +1,58 @@
+﻿using H1Z1Bot.IO;
+using H1Z1Bot.WinAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace H1Z1Bot
+{
+    public partial class wMain : Form
+    {
+        private static readonly int COORD_X = 1444;
+        private static readonly int COORD_Y = 719;
+
+        private InputSimulator iosim = new InputSimulator();
+        private Random rand = new Random();
+
+        public wMain()
+        {
+            InitializeComponent();
+            this.Icon = H1Z1Bot.Properties.Resources.icon;
+
+            this.lConnection.Text = "Desconectado";
+
+            HotkeyManager.Add(new HotkeyHandler(User32.VirtualKey.VK_F10, User32.ModifierKeys.None, OnF10Pressed));
+            HotkeyManager.Add(new HotkeyHandler(User32.VirtualKey.VK_F11, User32.ModifierKeys.None, OnF11Pressed));
+        }
+
+        private void OnF10Pressed(User32.VirtualKey key, User32.ModifierKeys modkey)
+        {
+            
+        }
+
+        private void OnF11Pressed(User32.VirtualKey key, User32.ModifierKeys modkey)
+        {
+        }
+
+        private void SendCode(uint code)
+        {
+
+            iosim
+                .Keyboard
+                .KeyPress(WindowsInput.Native.VirtualKeyCode.VK_E)
+                .Sleep(rand.Next(1000, 2000))
+                .TextEntry(code.ToString())
+                .Sleep(rand.Next(1000, 2000))
+                .Mouse
+                .MoveMouseTo(COORD_X, COORD_Y)
+                .LeftButtonClick()
+                .Sleep(rand.Next(1000, 2000));
+        }
+    }
+}
